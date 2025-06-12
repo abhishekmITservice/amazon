3d0c7d4fc4c25884707d0f167ddd68ed0c08c5e4
abhiGithubIT <128072329+abhiGithubIT@users.noreply.github.com>
2025-06-12 18:45:55 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -7,4 +7,5 @@ echo "You can now review the changes."
 echo "Thank you for reviewing the code."
 echo "Have a great day!"
 echo "This is a test for the pull request."
-echo "Please check the changes made in this script."
+echo "Please check the changes made in this script." 
+i have changed this
